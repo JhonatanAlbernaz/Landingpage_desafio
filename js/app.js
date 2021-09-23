a7d2a8fdff265b2b55b9cc00565c62d48d7e06d7
JhonatanAlbernaz <jhonatanalbernaz124@gmail.com>
2021-09-23 09:32:47 -0300

    Landingpage desafio em andamento

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "-27px";
-    document.getElementById("navbar").style.bottom = "256px";
+    document.getElementById("navbar").style.bottom = "254px";
     document.getElementById("navbar").style.padding = "1em";
   } else {
     document.getElementById("navbar").style.top = "0px";
-    document.getElementById("navbar").style.bottom = "230px";
+    document.getElementById("navbar").style.bottom = "30px";
     document.getElementById("navbar").style.padding = "0em";
   }
 }
